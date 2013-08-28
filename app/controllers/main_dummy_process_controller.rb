6ebb8af1e61bb0881456c876782a91337f41c46b
randysetiawan@cruxmultimedia.com <randysetiawan@cruxmultimedia.com>
2013-08-28 21:04:34 +0700

    adding some reminder to use module inside app folder

--- app/controllers/main_dummy_process_controller.rb
+++ app/controllers/main_dummy_process_controller.rb
@@ -1,4 +1,7 @@
 class MainDummyProcessController < ApplicationController
+  include CSharpParser
+
   def show
+      put_two_as_return
   end
 end
---
+++ app/modules/c_sharp_parser.rb
@@ -0,0 +1,14 @@
+module CSharpParser
+  class CSharp
+
+    def put_one_as_return
+        return 1
+    end
+
+  end
+
+  def put_two_as_return
+    return 2
+  end
+
+end
